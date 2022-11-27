9906eb495ad4ac58d3c8596a521f5a5788f6e000
fresh-avocado <gabriel.spranger@utec.edu.pe>
2022-11-27 09:37:25 -0500

    test: func that measures time

--- main.cpp
+++ main.cpp
@@ -2,32 +2,49 @@
 
 #include "BPlus.cpp"
 
+auto measureTime(const char* benchmarkName,
+                 const std::function<void(void)>& compute) -> void {
+  // NOLINTNEXTLINE
+  std::chrono::high_resolution_clock::time_point start =
+      std::chrono::high_resolution_clock::now();
+  compute();
+  // NOLINTNEXTLINE
+  std::chrono::high_resolution_clock::time_point end =
+      std::chrono::high_resolution_clock::now();
+
+  printf("%s took: %lf seconds\n", benchmarkName,
+         std::chrono::duration_cast<std::chrono::duration<double>>(end - start)
+             .count());
+}
+
 int main() {
   // setbuf(stdout, NULL);
   BPlusTree<3> bplus;
 
   int records[101];
-  for (int i = 1; i <= 100; i++) {
-    records[i] = i;
-    // bplus.insert(i, &records[i]);
-    // assert(bplus.search(i) == &records[i]);
-  }
+  measureTime("insertion time", [&] {
+    for (int i = 1; i <= 1'000'000; i++) {
+      // records[i] = i;
+      bplus.insert(i, records);
+      // assert(bplus.search(i) == &records[i]);
+    }
+  });
 
-  bplus.insert(1, &records[1]);
-  bplus.insert(20, &records[20]);
-  bplus.insert(49, &records[49]);
-  bplus.insert(21, &records[21]);
-  bplus.insert(11, &records[11]);
-  bplus.insert(9, &records[9]);
+  // bplus.insert(1, &records[1]);
+  // bplus.insert(20, &records[20]);
+  // bplus.insert(49, &records[49]);
+  // bplus.insert(21, &records[21]);
+  // bplus.insert(11, &records[11]);
+  // bplus.insert(9, &records[9]);
 
-  printf("inorder string: %s\n", bplus.inorderString().c_str());
+  // printf("inorder string: %s\n", bplus.inorderString().c_str());
   printf("height: %d\n", bplus.getHeight());
-  printf("root: %s\n", bplus.getRoot().c_str());
+  // printf("root: %s\n", bplus.getRoot().c_str());
 
-  for (auto i : bplus.range(SearchCriteria::GT, 9)) {
-    printf("%d,", *(int*)i);
-  }
-  printf("\n");
+  // for (auto i : bplus.range(SearchCriteria::GT, 9)) {
+  //   printf("%d,", *(int*)i);
+  // }
+  // printf("\n");
 
   return 0;
 }
